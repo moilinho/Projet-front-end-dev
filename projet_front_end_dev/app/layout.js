3d9601acf46eb4cb2031b8b40efb5795dab2ffbe
moilinho <moilim.abdallah@edu.univ-paris13>
2025-01-20 17:03:49 +0100

    ajout de lignes dans layout pour configurer redux

--- projet_front_end_dev/app/layout.js
+++ projet_front_end_dev/app/layout.js
@@ -1,6 +1,19 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Provider } from 'react-redux';
+import store from './store/store';
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Provider store={store}>
+          {children}
+        </Provider>
+      </body>
+    </html>
+  );
+}
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
