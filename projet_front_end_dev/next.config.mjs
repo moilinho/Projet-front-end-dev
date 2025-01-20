c5e50ad42a1b23d8782a1f2796346107aa1576db
moilinho <moilim.abdallah@edu.univ-paris13>
2025-01-20 19:16:43 +0100

    réglage du soucis avec la commande npm run dev

--- projet_front_end_dev/next.config.mjs
+++ projet_front_end_dev/next.config.mjs
@@ -2,7 +2,6 @@ import i18n from './i18n.js';
 
 const nextConfig = {
   reactStrictMode: true,
-  i18n, // Ajout de la configuration i18n
 };
 
-module.exports = nextConfig;
+export default nextConfig;
