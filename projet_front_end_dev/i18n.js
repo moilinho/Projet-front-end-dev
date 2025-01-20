334b71b38c87347e6a1aa2c36ce624e3b386cb4d
moilinho <moilim.abdallah@edu.univ-paris13>
2025-01-20 17:20:37 +0100

    configuration de i18n

---
+++ projet_front_end_dev/i18n.js
@@ -0,0 +1,9 @@
+const i18nConfig = {
+    i18n: {
+      defaultLocale: 'fr', // Langue par défaut
+      locales: ['fr', 'en'], // Langues supportées
+    },
+  };
+  
+  module.exports = i18nConfig;
+  
--- projet_front_end_dev/next.config.mjs
+++ projet_front_end_dev/next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { i18n } = require('./i18n');
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  i18n, // Ajout de la configuration i18n
+};
+
+module.exports = nextConfig;
