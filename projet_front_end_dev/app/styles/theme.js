4b3adceda7f473573f1e7e7d43dfeb4a88c03a2d
moilinho <moilim.abdallah@edu.univ-paris13>
2025-01-20 17:11:25 +0100

    theme qui harmonise les interface

---
+++ projet_front_end_dev/app/styles/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+    token: {
+      colorPrimary: '#1890ff', // Couleur principale
+      colorSecondary: '#52c41a', // Couleur secondaire
+      borderRadius: 8, // Rayon des bordures
+      fontSizeBase: 14, // Taille de police de base
+    },
+    components: {
+      Button: {
+        borderRadius: 8,
+      },
+    },
+  };
+  
+  export default theme;
